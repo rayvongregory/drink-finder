dcb382edbdda13e4a17c0410d2ec92e603b479f7
rayvongregory <65968223+rayvongregory@users.noreply.github.com>
2022-02-21 23:17:22 -0500

    Update drink-finder.js

--- Cocktail Recipe/drink-finder.js
+++ Cocktail Recipe/drink-finder.js
@@ -1,11 +1,10 @@
-const searchBtn = document.getElementById("banner___search-btn");
-const searchInput = document.getElementById("banner___search-input");
-const recipes = document.getElementById("recipes");
-const resultsH2 = document.getElementById("results___text-h2");
-const resultsP = document.getElementById("results___text-p");
+const searchBtn = document.getElementById("search-btn")
+const searchInput = document.getElementById("search-input")
+const recipes = document.getElementById("recipes")
+const resultsP = document.getElementById("results___text")
 
 const getRecipe = (e) => {
-   if (e.key !== "Enter" && e.key) {
+  if (e.key !== "Enter" && e.key) {
     return
   }
   let val = searchInput.value.trim()
@@ -14,80 +13,75 @@ const getRecipe = (e) => {
       .then((response) => response.json())
       .then((data) => {
         if (!data.drinks) {
-          resultsH2.innerHTML = "Sorry, there are no results.";
-          resultsH2.style.margin = "1em auto 0";
-          resultsP.innerHTML = "Try another search!";
-          recipes.innerHTML = "";
-          searchInput.value = "";
+          resultsP.innerHTML = `There are no results for "${val}". <br/> Try another search.`
+          resultsP.classList.add("no-results")
+          recipes.replaceChildren()
+          searchInput.value = ""
+          searchInput.focus()
         } else {
-          resultsH2.innerHTML = "Your results";
-          resultsH2.style.margin = "1em auto";
-          resultsP.innerHTML = "";
-          let html = "";
+          resultsP.innerHTML = `Your results for "${val}"`
+          if (resultsP.classList.contains("no-results")) {
+            resultsP.classList.remove("no-results")
+          }
+          let html = ""
           data.drinks.forEach((drink) => {
-            let ingredients = [];
-            let measurements = [];
+            let ingredients = []
+            let measurements = []
             for (let key in drink) {
               if (`${key}`.startsWith("strIngredient") && drink[key]) {
-                ingredients.push(drink[key]);
+                ingredients.push(drink[key])
               } else if (`${key}`.startsWith("strMeasure") && drink[key]) {
-                measurements.push(drink[key]);
+                measurements.push(drink[key])
               }
             }
             for (let i = 0; i < ingredients.length; i++) {
               ingredients[i] = `<li>${[measurements[i], ingredients[i]].join(
                 " "
-              )}</li>`;
+              )}</li>`
             }
-
             html += ` <div class="recipe___card-container">
             <div class="recipe___card">
               <div class="recipe___card-front">
                 <img src="${drink.strDrinkThumb}" alt="">
-                <div class="recipe___button">
-                  <button>Get recipe</button>
-                </div>
+                <button class="recipe___button">Get recipe</button>
                 <div class="recipe___card-name">     
-                <h2>${drink.strDrink}</h2>
+                <p>${drink.strDrink}</p>
                 </div> 
               </div> 
               <div class="recipe___card-back">
-                <h2>${drink.strDrink}</h2>
-                <button class="recipe___exit">X</button>
+                <p>${drink.strDrink}</p>
+                <button class="recipe___exit"><i class="fa fa-times"></i></button>
                 <p class="underline">Ingredients:</p>
                 <ul>
                 ${ingredients.join("")}
                 </ul>
                 </br>
-                <p class="paddingBottom"><span class="underline">Instructions</span>: ${
+                <p><span class="underline">Instructions</span>: ${
                   drink.strInstructions
                 }</p>
-            
               </div>     
             </div>
-          </div>`;
-            recipes.innerHTML = html;
-          });
+          </div>`
+            recipes.innerHTML = html
+          })
         }
-      });
+      })
   }
-};
+}
 
 const showBack = (e) => {
-  if (e.target.parentElement.classList[0] === "recipe___button") {
-    e.target.parentElement.parentElement.parentElement.classList.toggle(
-      "rotateY"
-    );
+  if (e.target.classList.contains("recipe___button")) {
+    e.target.parentElement.parentElement.classList.toggle("rotateY")
   }
-};
+}
 
 const showFront = (e) => {
-  if (e.target.classList[0] === "recipe___exit") {
-    e.target.parentElement.parentElement.classList.toggle("rotateY");
+  if (e.target.classList.contains("recipe___exit")) {
+    e.target.parentElement.parentElement.classList.toggle("rotateY")
   }
-};
+}
 
-document.addEventListener("click", getRecipe);
-document.addEventListener("keypress", getRecipe);
-document.addEventListener("click", showBack);
-document.addEventListener("click", showFront);
+searchBtn.addEventListener("click", getRecipe)
+searchInput.addEventListener("keyup", getRecipe)
+document.addEventListener("click", showBack)
+document.addEventListener("click", showFront)
